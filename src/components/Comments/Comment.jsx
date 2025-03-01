f92e12d989309e297b702aa9c463b676e0c4107e
Vishal Pandit <152159518+TrooperDG@users.noreply.github.com>
2025-03-01 18:09:14 +0530

    added is authentication on edit-delete comment

--- src/components/Comments/Comment.jsx
+++ src/components/Comments/Comment.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { getTimeAgo } from "../../utility";
+import { useSelector } from "react-redux";
 import databaseService from "../../appwrite/database";
 databaseService;
 
@@ -7,6 +8,10 @@ function Comment({ comment, handleDeleteComment, handleEditComment }) {
   const [isEditDeleteOpen, setIsEditDeleteOpen] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [editComment, setEditComment] = useState(comment.comment);
+
+  const userData = useSelector((state) => state.auth.userData);
+  const isAuthor =
+    comment && userData ? comment.userId === userData.$id : false;
 
   function handleEdit() {
     setIsEditDeleteOpen(false);
@@ -95,63 +100,65 @@ function Comment({ comment, handleDeleteComment, handleEditComment }) {
           )}
         </div>
       </div>
-      <div className="features relative ">
-        {!isEdit && (
-          <button
-            className=" three-dots py-1 "
-            onClick={() => setIsEditDeleteOpen((prev) => !prev)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#6a7282"
-            >
-              <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
-            </svg>
-          </button>
-        )}
-        {isEditDeleteOpen && (
-          <div
-            className={`rounded-sm overflow-hidden bg-gray-50 duration-100  absolute top-0 -left-20 `}
-          >
+      {isAuthor && (
+        <div className="features relative ">
+          {!isEdit && (
             <button
-              id="edit-comment"
-              className=" p-1 mr-2  hover:bg-gray-100 active:bg-gray-100"
-              onClick={handleEdit}
+              className=" three-dots py-1 "
+              onClick={() => setIsEditDeleteOpen((prev) => !prev)}
             >
               <svg
-                className="w-6 h-6"
                 xmlns="http://www.w3.org/2000/svg"
                 height="24px"
                 viewBox="0 -960 960 960"
                 width="24px"
                 fill="#6a7282"
               >
-                <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
+                <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
               </svg>
             </button>
+          )}
+          {isEditDeleteOpen && (
+            <div
+              className={`rounded-sm overflow-hidden bg-gray-50 duration-100  absolute top-0 -left-20 `}
+            >
+              <button
+                id="edit-comment"
+                className=" p-1 mr-2  hover:bg-gray-100 active:bg-gray-100"
+                onClick={handleEdit}
+              >
+                <svg
+                  className="w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#6a7282"
+                >
+                  <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
+                </svg>
+              </button>
 
-            <button
-              id="delete-comment"
-              className="  p-1  hover:bg-gray-100 active:bg-gray-100 "
-              onClick={() => handleDeleteComment(comment.$id)}
-            >
-              <svg
-                className="w-6 h-6"
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#6a7282"
+              <button
+                id="delete-comment"
+                className="  p-1  hover:bg-gray-100 active:bg-gray-100 "
+                onClick={() => handleDeleteComment(comment.$id)}
               >
-                <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-              </svg>
-            </button>
-          </div>
-        )}
-      </div>
+                <svg
+                  className="w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#6a7282"
+                >
+                  <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                </svg>
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 }
